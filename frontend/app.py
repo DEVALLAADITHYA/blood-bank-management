116f0eab3402db2087eb6113e52522b224cb1368
DEVALLA ADITHYA <adithyadevalla@gmail.com>
2026-03-22 15:50:14 +0530

    Update BASE URL for blood bank management

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import requests
 import pandas as pd
 import plotly.express as px
 
-BASE = "https://blood-bank-management-czmd.onrender.com"
+BASE = "https://blood-bank-management-1-nrdt.onrender.com"
 
 st.set_page_config(page_title="Blood Bank", layout="wide")
 
